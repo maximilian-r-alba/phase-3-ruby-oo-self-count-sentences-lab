ef851804b1380bf83f56be48a4855510ce1d26a6
Maximilian Rex Alba <albam476@gmail.com>
2022-12-10 16:20:04 -0500

    commit tree 5b00913feaf1cdd79b7ef0c359ba3580956f4ba2

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # binding.pry
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/\?|\!|\./).filter {|sentence| sentence.length>0}.length
   end
 end
+binding.pry
+0
